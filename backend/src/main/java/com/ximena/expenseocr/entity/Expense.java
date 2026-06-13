9a8872c74513bba5b371afaec90311778b3591a9
gmonie <xime_sanchez45@ciencias.unam.mx>
2026-06-12 19:08:21 -0600

    docs(Expense.java): Documentación para la entidad Expense.

--- backend/src/main/java/com/ximena/expenseocr/entity/Expense.java
+++ backend/src/main/java/com/ximena/expenseocr/entity/Expense.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+/**
+ * Clase entidad que representa un gasto registrado por el usuario.
+ */
 @Entity
 @Table(name = "expenses")
 @Getter
@@ -18,13 +21,14 @@ public class Expense {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    /*Identificador único del gasto.*/
     private UUID id;
-
+    /*Nombre de la tienda donde se realizó el gasto.*/
     private String store;
-
+    /*Cantidad total del gasto.*/
     private BigDecimal amount;
-
+    /*Fecha en la que se realizó el gasto.*/
     private LocalDate purchaseDate;
-
+    /*Categoría del gasto.*/
     private String category;
 }
