0cd6087c9f77040c1bcfb7f88b0465c1a399d9da
gmonie <xime_sanchez45@ciencias.unam.mx>
2026-06-12 18:46:23 -0600

    ExpenseController documentado

--- backend/src/main/java/com/ximena/expenseocr/controller/ExpenseController.java
+++ backend/src/main/java/com/ximena/expenseocr/controller/ExpenseController.java
@@ -15,11 +15,23 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/expenses")
-@RequiredArgsConstructor
+@RequiredArgsConstructor 
+
+/**
+    * Esta clase es la encargada de manejar las solicitudes HTTP que se
+    * relacionan a los gastos, además de definir los endpoints para las
+    * operaciones CRUD y utiliza ExpenseService para manejar la lógica
+    * de negocio.
+**/
 
 public class ExpenseController {
     private final ExpenseService expenseService;
 
+    /**
+        * Método para crear un nuevo gasto.
+        * @param request es un objeto que contiene los datos necesarios para crear un gasto. 
+        * @return un objeto ExpenseResponse que representa el gasto creado.
+    */
    @PostMapping 
    public ExpenseResponse create(
             @RequestBody ExpenseRequest request
@@ -27,11 +39,22 @@ public class ExpenseController {
          return expenseService.create(request);
     }
 
+    /**
+        * Método para obtener todos los gastos.
+        * @return una lista de objetos ExpenseResponse que representan todos los gastos almacenados.
+    */
     @GetMapping
     public List<ExpenseResponse> findAll() {
         return expenseService.findAll();
     }
 
+    /**
+     * Método para actualizar un gasto existente.
+     * @param id identificador único del gasto a actualizar.
+     * @param request objeto que contiene los datos necesarios 
+     * para actualizar el gasto.
+     * @return objeto de tipo ExpenseResponse que es el gasto actualizado.
+     */
     @PutMapping("/{id}")
     public ExpenseResponse update(
         @PathVariable UUID id,
@@ -40,6 +63,13 @@ public class ExpenseController {
         return expenseService.update(id, request);
     }
 
+    /**
+     * Método para obtener un gasto por su ID.
+     * @param id identificador único del gasto que se quiere buscar.
+     * @return objeto de tipo ExpenseResponse que representa el gasto 
+     * encontrado.
+     * @throws ExpenseNotFoundException si no se encuentra un gasto con el ID proporcionado.
+     */
     @GetMapping("/{id}") 
     public ExpenseResponse findById( 
         @PathVariable UUID id
@@ -48,6 +78,10 @@ public class ExpenseController {
      }
 
     
+    /**
+     * Método para eliminar un gasto por su ID.
+     * @param id identificador único del gasto que se quiere eliminar.
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
